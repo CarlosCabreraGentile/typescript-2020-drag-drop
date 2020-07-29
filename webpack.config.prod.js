d843a8488f9b095cb59c5a219be98c119d066039
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2020-07-29 13:29:35 -0300

    157: production setup webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,16 @@
 const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin'); // clean dist folder before build
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     //look for the starter point in this case is app.js, look all the imports
     entry: './src/app.ts',
     output: {
         //filename: 'bundle.[contenthash].js' in case want to add a hash
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
-    devtool: 'inline-source-map',
+    devtool: 'none',
     module: {
         rules: [
             {
@@ -22,5 +22,8 @@ module.exports = {
     },
     resolve: {
         extensions: ['.ts', '.js'] //look for this extensions files and bundle all files with these extensions
-    }
+    },
+    plugins: [ //applies to the general workflow
+        new CleanPlugin.CleanWebpackPlugin()
+    ]
 };
